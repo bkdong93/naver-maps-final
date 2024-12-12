b396c11fb5df2a81ca1307d03ff1d09b1d270647
bkdong93 <bkdong93@naver.com>
2024-12-12 16:49:50 +0900

    update index.js

--- naver-maps-gpts/api/index.ts
+++ naver-maps-gpts/api/index.ts
@@ -1,6 +1,6 @@
-// const express = require("express");
-// const axios = require("axios");
-// const app = express();
+const express = require("express");
+const axios = require("axios");
+const app = express();
 
 app.get("/", (req, res) => res.send("Server is now running"));
 
